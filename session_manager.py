6a2cf949a5a620ce3558d6b0c742b329803deaac
GamblingStar2025 <newspoolestate@gmail.com>
2025-05-22 23:04:57 +0200

    Delete session_manager.py

--- session_manager.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-def check_login():
-    if not st.session_state.get("is_logged_in"):
-        st.warning("🔒 Du bist nicht eingeloggt. Weiterleitung zum Login...")
-        st.switch_page("pages/login.py")
-        st.stop()
