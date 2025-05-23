b6e1519de636e30076ff662a39b4a431f5d4831f
GamblingStar2025 <newspoolestate@gmail.com>
2025-05-23 23:12:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,5 @@ import streamlit as st
 st.set_page_config(page_title="EuroGenius Start", layout="centered")
 st.title("✨ Willkommen bei EuroGenius")
 
-if 'page' not in st.session_state:
-    st.session_state.page = 'start'
-
-if st.button("📊 Analysen anzeigen"):
-    st.session_state.page = 'upload'
-    st.experimental_rerun()
-
-if st.session_state.page == 'upload':
-    st.switch_page("pages/csv_upload.py")
+if st.button("📊 Zur Analysezentrale"):
+    st.switch_page("pages/analyse.py")
